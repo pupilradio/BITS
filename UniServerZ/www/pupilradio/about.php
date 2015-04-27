2a2a4ef10d0a5b0fe5f3e4b183079b31766c9584
Daniel Popovic <s3423072@student.rmit.edu.au>
2015-04-27 10:42:05 +1000

    genres, about, request pages created and linked

---
+++ UniServerZ/www/pupilradio/about.php
@@ -0,0 +1,45 @@
+<?php ob_start("ob_gzhandler"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html xmlns="http://www.w3.org/TR/REC-html40">
+    <head>
+        <link type="text/css" rel="stylesheet" href="css/radio_style.css"/>
+        <title></title>
+    </head>
+    <body>
+        <div class='headercontainer'>
+            <div class='header'>
+                <div id="hmenu">
+                    <ul>
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="about.php">AboutUs</a>
+                        </li>
+                        <li>
+                            <a href="request.php">Request</a>
+                        </li>
+                        <li>
+                            <a href="genres.php">Genre</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class='mainbodycontainer'>
+                
+            </div>
+                <div class = 'footercontainer'>
+                    <div class='footer'>
+                        <p class="copyright">Copyright &copy; 2015 Pupil Radio</p>
+                        <p class="links">
+                            <a href="index.php">home</a> | 
+                            <a href="about.php">aboutus </a> | 
+                            <a href="#request.php">request</a> | 
+                            <a href="genres.php">genre</a>
+                        </p>
+                    </div>
+                </div>
+            </body>
+        </html>
+<?php ob_flush(); ?>
--- UniServerZ/www/pupilradio/index.php
+++ UniServerZ/www/pupilradio/index.php
@@ -1,8 +1,7 @@
 <?php ob_start("ob_gzhandler"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-<html
-    xmlns="http://www.w3.org/TR/REC-html40">
+<html xmlns="http://www.w3.org/TR/REC-html40">
     <head>
         <link type="text/css" rel="stylesheet" href="css/radio_style.css"/>
         <title></title>
@@ -13,16 +12,16 @@
                 <div id="hmenu">
                     <ul>
                         <li>
-                            <a href="#">Home</a>
+                            <a href="index.php">Home</a>
                         </li>
                         <li>
-                            <a href="#">AboutUs</a>
+                            <a href="about.php">AboutUs</a>
                         </li>
                         <li>
-                            <a href="#">Request</a>
+                            <a href="request.php">Request</a>
                         </li>
                         <li>
-                            <a href="#">Genre</a>
+                            <a href="genres.php">Genre</a>
                         </ul>
                     </div>
                 </div>
@@ -42,10 +41,10 @@
                     <div class='footer'>
                         <p class="copyright">Copyright &copy; 2015 Pupil Radio</p>
                         <p class="links">
-                            <a href="#">home</a> | 
-                            <a href="#">aboutus </a> | 
-                            <a href="#">request</a> | 
-                            <a href="#">genre</a>
+                            <a href="index.php">home</a> | 
+                            <a href="about.php">aboutus </a> | 
+                            <a href="request.php">request</a> | 
+                            <a href="genres.php">genre</a>
                         </p>
                     </div>
                 </div>
